58a59e35452336a052b33950bb5dcca511723b1a
CATT-L <github@catt-l.com>
2022-04-06 23:05:20 +0800

    Render markdown to html, based on .

--- src/components/editor/index.vue
+++ src/components/editor/index.vue
@@ -1,32 +1,84 @@
 <template>
-  <div ref="editor" class="editor" contenteditable="true">
-    <div>Hello,world</div>
+  <div style="height: 100%">
+    <el-container>
+      <el-header>
+        <div style="display: flex">
+          <div style="flex-grow: 1">
+            <h1>Markdown Editor</h1>
+          </div>
+          <div style="flex-grow: 0"></div>
+        </div>
+      </el-header>
+      <el-container>
+        <!-- <el-aside width="200px">Aside</el-aside> -->
+        <el-container>
+          <el-main>
+            <el-row :gutter="15" style="height: 100%">
+              <el-col :span="12">
+                <el-input
+                  type="textarea"
+                  input-style="
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+                    outline: none;
+                    resize: none;
+                    padding: 5px;
+                  "
+                  v-model="source"
+                ></el-input>
+              </el-col>
+              <el-col :span="12">
+                <div style="padding: 5px; height: 100%">
+                  <div
+                    class="result"
+                    style="
+                      height: 100%;
+                      border: solid 1px #dcdfe6;
+                      border-radius: 5px;
+                      padding: 5px;
+                    "
+                    v-html="html"
+                  ></div>
+                </div>
+              </el-col>
+            </el-row>
+          </el-main>
+          <!-- <el-footer>Footer</el-footer> -->
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import MarkdownIt from "markdown-it";
+import "@/assets/themes/github.css";
+
 export default {
   data() {
-    return {};
+    return {
+      source: "",
+      html: "",
+      mdit: new MarkdownIt(),
+    };
   },
-  mounted() {
-    console.log(this.$refs["editor"]);
+  computed: {
+    html() {
+      return this.mdit.render(this.source);
+    },
   },
-  unmounted() {},
+  methods: {},
 };
 </script>
 
-
-<style scoped>
-
-.editor {
-
-  border: solid 1px red;
-  
-  outline: none;
-  margin: 0 auto;
-  padding: 50px 10px;
-  width: 1000px;
+<style>
+.el-container {
+  height: 100%;
 }
 
+.result img {
+  max-width: 100%;
+}
 </style>
+
