631fff5866e9647fcd3f92421cda3af904dd2bfd
CATT-L <github@catt-l.com>
2022-04-07 16:36:21 +0800

    layout for main area

--- src/components/editor/index.vue
+++ src/components/editor/index.vue
@@ -2,52 +2,58 @@
   <div style="height: 100%">
     <el-container>
       <el-header>
-        <div style="font-size: 2rem;">Markdown Editor</div>
+        <div style="font-size: 2rem">Markdown Editor</div>
       </el-header>
       <el-container>
         <el-aside width="280px">
-          
-          <div style="background: #eee; height: 100%;">
+          <div style="background: #eee; height: 100%">
             <project-tree></project-tree>
           </div>
-
-
         </el-aside>
         <el-container>
-          <el-main>
+          <el-main
+            style="
+              position: relative;
+              width: 100%;
+              height: 100%;
+              padding: 0;
+              overflow: hidden;
+            "
+          >
+            <div
+              style="
+                position: absolute;
+                height: 100%;
+                width: 50%;
+                left: 0;
+                top: 0;
+              "
+            >
+              <textarea
+                style="
+                  display: block;
+                  width: 100%;
+                  height: 100%;
+                  resize: none;
+                  outline: none;
+                  border: none;
+                "
+                v-model="source"
+              ></textarea>
+            </div>
 
-            <el-button>button</el-button>
-            
-            <el-row :gutter="15" style="height: 100%">
-              <el-col :span="12">
-                <el-input
-                  type="textarea"
-                  input-style="
-                    display: block;
-                    width: 100%;
-                    height: 100%;
-                    outline: none;
-                    resize: none;
-                    padding: 5px;
-                  "
-                  v-model="source"
-                ></el-input>
-              </el-col>
-              <el-col :span="12">
-                <div style="padding: 5px; height: 100%">
-                  <div
-                    class="result"
-                    style="
-                      height: 100%;
-                      border: solid 1px #dcdfe6;
-                      border-radius: 5px;
-                      padding: 5px;
-                    "
-                    v-html="html"
-                  ></div>
-                </div>
-              </el-col>
-            </el-row>
+            <div
+              style="
+                position: absolute;
+                height: 100%;
+                width: 50%;
+                top: 0;
+                right: 0;
+                overflow-y: auto;
+              "
+            >
+              <div class="result" style="" v-html="html"></div>
+            </div>
           </el-main>
           <!-- <el-footer>Footer</el-footer> -->
         </el-container>
@@ -62,12 +68,13 @@ import "@/assets/themes/github.css";
 
 export default {
   components: {
-    'project-tree': require('./project-tree').default,
+    "project-tree": require("./project-tree").default,
   },
   data() {
     return {
       source: "",
       mdit: new MarkdownIt(),
+      timerAutoSave: null,
     };
   },
   computed: {
@@ -75,7 +82,34 @@ export default {
       return this.mdit.render(this.source);
     },
   },
-  methods: {},
+  mounted() {
+    this.handleLoad();
+
+    this.timerAutoSave = setInterval(() => {
+      this.handleSave();
+    }, 5e3);
+  },
+
+  unmounted() {
+    clearInterval(this.timerAutoSave);
+    this.timerAutoSave = null;
+    this.handleSave();
+  },
+  methods: {
+    handleLoad() {
+      try {
+        var source = JSON.parse(localStorage.source);
+      } catch (e) {
+        var source = "";
+      }
+
+      this.source = source;
+    },
+
+    handleSave() {
+      localStorage.source = JSON.stringify(this.source);
+    },
+  },
 };
 </script>
 
@@ -84,7 +118,26 @@ export default {
   height: 100%;
 }
 
+.result {
+  background: #fff;
+  border-radius: 5px;
+  padding: 15px;
+}
+
 .result img {
   max-width: 100%;
+  display: block;
+  margin: 0 auto;
+}
+
+.result pre {
+  width: 100%;
+  background: #f8f8f8;
+  padding: 5px 10px;
+}
+
+.result pre code {
+  background: none;
+  border: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ContextMenu from '@imengyu/vue3-context-menu';
 import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
 import '@/vue3-context-menu.css';
 import "element-plus/dist/index.css";
+import 'flex.css'
 import App from "./App.vue";
 
 const app = createApp(App);
