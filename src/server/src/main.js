37b947589e6f27c2381cdb841d3eb262837a06d5
CATT-L <github@catt-l.com>
2022-04-23 21:21:16 +0800

    koa and socket.io server

--- src/server/src/main.js
+++ src/server/src/main.js
@@ -1,10 +1,36 @@
-import http from 'http';
+let PORT = 3000;
 
-// 创建HTTP服务
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello,world!");
+import Http from "http";
+import Koa from "koa";
+import SocketIO from "socket.io";
+
+const api = new Koa();
+const server = Http.createServer(api.callback());
+const io = SocketIO(server);
+
+api.use(async (ctx, next) => {
+  if (ctx.request.path === "/") {
+    ctx.response.body = "index page";
+  } else {
+    await next();
+  }
 });
-server.listen(8080);
 
-console.log("Server started!");
+io.of("/").on("connection", (socket) => {
+  console.log("A user connected.");
+
+  socket.emit("echo", "Hello, world!");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected.");
+  });
+
+  socket.on("echo", (msg) => {
+    console.log("Echo from client:" + msg);
+    socket.emit("echo", msg);
+  });
+});
+
+server.listen(PORT);
+console.log("Server start listening on port:" + PORT);
+
