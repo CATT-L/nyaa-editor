b736fd86fff4978d3f9fb87297ad23d0a3a5feb3
CATT-L <github@catt-l.com>
2022-04-23 17:19:55 +0800

    move web source code to src/web/src

---
+++ src/web/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("@vue/cli-service");
+
+process.env.VUE_APP_NAME = require("./package.json").name;
+process.env.VUE_APP_VERSION = require("./package.json").version;
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || "/",
+  lintOnSave: false,
+});
