50c4ecdcd4bb30114906e770d94613651c3c1332
CATT-L <github@catt-l.com>
2022-04-23 19:36:39 +0800

    electron code

--- src/desktop/main.js
+++ src/desktop/main.js
@@ -2,13 +2,22 @@ const { app, BrowserWindow, Tray, Menu } = require("electron");
 const path = require("path");
 
 app.whenReady().then(async () => {
-  // 服务器
-  const server = new BrowserWindow({
+  // 后端服务
+  const backendServer = new BrowserWindow({
     show: false,
     webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
 
-  await server.loadFile(path.join(__dirname, "server.html"));
+  await backendServer.loadFile(path.join(__dirname, "server.html"));
+
+  // // 前端服务
+  // const frontendServer = new BrowserWindow({
+  //   show: false,
+  //   webPreferences: {},
+  // });
+
+  var frontendUrl = "http://localhost:24061";
+  var backendUrl = "http://localhost:8080";
 
   // 客户端
   const client = new BrowserWindow({
@@ -16,28 +25,9 @@ app.whenReady().then(async () => {
     height: 600,
   });
 
-  // client.loadURL("http://localhost:8080");
-  client.loadFile(path.join('./dist/index.html'));
+  client.loadURL(frontendUrl);
 
   client.on("close", function (e) {
-    e.preventDefault();
-    client.hide();
+    app.quit(-1);
   });
-
-  const tray = new Tray(path.join(__dirname, "assets/logo.png"));
-
-  tray.on("click", function () {
-    client.show();
-  });
-
-  tray.setContextMenu(
-    Menu.buildFromTemplate([
-      {
-        label: "退出",
-        click: () => {
-          app.exit();
-        },
-      },
-    ])
-  );
 });
